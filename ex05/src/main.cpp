0b059240911eaa81ccc26f4f358ad0cdccf01a0e
JustShush <marquesdinis.p@gmail.com>
2024-03-27 11:16:55 +0000

    ex05 This is somewhat usefull, lets see in the future if i will ever use it

---
+++ ex05/src/main.cpp
@@ -0,0 +1,13 @@
+#include "../inc/Harl.hpp"
+
+int	main(void) {
+
+	Harl object;
+
+	object.complain("DEBUG");
+	object.complain("INFO");
+	object.complain("WARNING");
+	object.complain("ERROR");
+
+	return (0);
+}

68d84384c90b4b71ae1976e88a2370f092694d03
JustShush <marquesdinis.p@gmail.com>
2024-04-01 11:05:08 +0100

    better code

--- ex00/src/ClapTrap.cpp
+++ ex00/src/ClapTrap.cpp
@@ -1,25 +1,20 @@
 #include "../inc/ClapTrap.hpp"
 
-// ---------------------- Orthodox Canonical Form -----------------------------
-ClapTrap::ClapTrap() : _hp(10), _energy(10), _attack(0)
-{
+ClapTrap::ClapTrap() : _hp(10), _energy(10), _attack(0) {
 	std::cout << GREEN << "Default Constructor Called" << RESET << "\n";
 }
 
-ClapTrap::ClapTrap(std::string name) : _hp(10), _energy(10), _attack(0)
-{
+ClapTrap::ClapTrap(std::string name) : _hp(10), _energy(10), _attack(0) {
 	_name = name;
 	std::cout << ORANGE << name << GREEN << " Constructor Called" << RESET << "\n";
 }
 
-ClapTrap::ClapTrap(const ClapTrap &copy)
-{
+ClapTrap::ClapTrap(const ClapTrap &copy) {
 	*this = copy;
 	std::cout << GREEN << "Copy Constructor Called" << RESET << "\n";
 }
 
-ClapTrap::~ClapTrap()
-{
+ClapTrap::~ClapTrap() {
 	std::cout << RED << "Destructor Called" << RESET << "\n";
 }
 

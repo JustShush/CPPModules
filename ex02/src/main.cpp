f87843c8a60bf78f2bb0f97531d0e47717f3be94
JustShush <marquesdinis.p@gmail.com>
2024-09-11 11:50:56 +0100

    making sure the main is the same has in the subject

--- ex02/src/main.cpp
+++ ex02/src/main.cpp
@@ -2,21 +2,29 @@
 
 int main() {
 	MutantStack<int> mstack;
+
 	mstack.push(5);
 	mstack.push(17);
+
 	std::cout << "TOP: " << mstack.top() << std::endl;
+	
 	mstack.pop();
+	
 	std::cout << "SIZE: "<< mstack.size() << std::endl;
+	
 	mstack.push(3);
 	mstack.push(5);
 	mstack.push(737);
-
+	//[...]
 	mstack.push(0);
+	
 	MutantStack<int>::itM	it = mstack.begin();
 	MutantStack<int>::itM	ite = mstack.end();
+	
 	++it;
 	--it;
-	while (it != ite) {
+	while (it != ite)
+	{
 		std::cout << *it << std::endl;
 		++it;
 	}
